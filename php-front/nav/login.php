a6cb41995e2f80479bf7e7dd0af26237131ed292
Zeeshan <zshanakhtar@oulook.com>
2018-09-23 05:20:10 +0530

    Navigation index

---
+++ php-front/nav/login.php
@@ -0,0 +1,18 @@
+
+<!-- Navbar -->
+<nav class="z-navbar col-sm-12">
+    <div class="col-sm-1">
+        <img class="col-sm-12" src="img/logo.png" alt="KEC_logo">
+        <hr class="col-sm-12" style="opacity:0;"></hr>
+        <a href="http://in.linkedin.com/pub/krishna-engineering-college-official-page/ab/197/620" target="_blank"><img src="img/linkedin.png" height="30px"></a>
+        <a href="https://www.facebook.com/KEC2004" target="_blank"><img src="img/facebook.png" height="30px"></a>
+        <a href="http://youtube.com/c/KECGhaziabadOfficial" target="_blank"><img src="img/youtube.png" height="30px"></a>
+        <a href="https://twitter.com/KrishnaCollege" target="_blank"><img src="img/twitter.png" height="30px"></a>
+    </div>
+    <div class="col-sm-10">
+        <div class="text-center">
+            <h1>Krishna Engineering College</h1>
+            <p>Affiliated to Dr APJ Abdul Kalam Technical University, Approved by AICTE, Ministry of HRD, Govt. of India</p>
+        </div>
+    </div>
+</nav>

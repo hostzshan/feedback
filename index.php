733b59e91362c74fbf22e3b378eab580b76ff7dc
Zeeshan <zshanakhtar@oulook.com>
2018-09-23 03:32:45 +0530

    index modules updated

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if(isset($_SESSION['usertype'])) { // if already login
 else{
   $usertype='n';
   $nav_type='login';
-  $menu_items=array('token'=>'Generate Token','signin'=>'Sign In','contact'=>'Contact Administrator');
+  $menu_items=array('signin'=>'Sign In','register'=>'Request Access');
 }
 $canvas_paint=$usertype.'/token.php';
 

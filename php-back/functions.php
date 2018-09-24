e856511ae4914032d531f8f85a15fb9bcd3ed56f
Zeeshan <zshanakhtar@oulook.com>
2018-09-25 03:00:42 +0530

    separate functions file

---
+++ php-back/functions.php
@@ -0,0 +1,45 @@
+<?php
+function realescape($arr){
+    include "../php-back/".'connection.php';
+    foreach($arr as $x => $x_value)
+        $arr[$x]=mysqli_real_escape_string($conn, $x_value);
+    return $arr;
+}
+function insertintodb($table,$data){
+    include "../php-back/".'connection.php';
+    $keys=implode(",", array_keys($data) );
+    $values=implode("','", array_values($data) );
+    $FILLteam="INSERT INTO $table ($keys) values ('$values')";
+    echo $FILLteam;
+    if($conn->query($FILLteam))
+        return true;
+    else
+        return false;
+}
+function errordisplay($error_main,$error_desc){
+    echo '<div class="alert alert-danger"><strong>'.$error_main.'!</strong> '.$error_desc.'</div>';
+}
+function sendmail($to,$subject,$body){
+    $mail = new PHPMailer(); // create a new object
+    $mail->IsSMTP(); // enable SMTP
+    $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true; // authentication enabled
+    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465; // or 587
+    $mail->IsHTML(true);
+    $mail->Username = "authenticatorkaprakop";
+    $mail->Password = "asli!pass";
+    $mail->SetFrom("authenticatorkaprakop@gmail.com");
+    $mail->Subject = $subject;
+    $mail->Body = $body;
+    $mail->AddAddress($to);
+
+    if(!$mail->Send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    }
+    else {
+        echo "Message has been sent";
+    }
+}
+?>
--- php-back/submitregister.php
+++ php-back/submitregister.php
@@ -2,12 +2,12 @@
 include '..\PHPMailer\src\PHPMailer.php';
 include '..\PHPMailer\src\SMTP.php';
 include '..\PHPMailer\src\Exception.php';
+include 'functions.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 session_start();
-date_default_timezone_set("Asia/Calcutta"); 
-$app_id=date('dmHis');
+$error_main='Registration Failed!';
 
 if ($_SERVER['REQUEST_METHOD']=='POST'){
     $_POST=realescape($_POST);//escape single and double quotes using mysqli_real_escape
@@ -19,10 +19,12 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
         $data=array('username'=>$univ_roll,'pass'=>$hashandsalt,'usertype'=>'s','access_code'=>$access_code);
         if(insertintodb('regist',$data))
             sendmail('zshanakhtar@outlook.com','Registration Done Successfully','You have been successfully registered please contact administrator to activate account.');
+        else
+            errordisplay($error_main,'Server Error, please try again later.');
     }
     else
     {
-
+        errordisplay($error_main,'Invalid access code.');
     }
     //if( password_verify($pass, $hashandsalt) )
     //echo '<br>success<br>';
@@ -31,44 +33,5 @@ else{
 	header("Location:../index.php");
 }
 
-function realescape($arr){
-    include "../php-back/".'connection.php';
-    foreach($arr as $x => $x_value)
-        $arr[$x]=mysqli_real_escape_string($conn, $x_value);
-    return $arr;
-}
-function insertintodb($table,$data){
-    include "../php-back/".'connection.php';
-    $keys=implode(",", array_keys($data) );
-    $values=implode("','", array_values($data) );
-    $FILLteam="INSERT INTO $table ($keys) values ('$values')";
-    echo $FILLteam;
-    if($conn->query($FILLteam))
-        return true;
-    else
-        return false;
-}
-function sendmail($to,$subject,$body){
-    $mail = new PHPMailer(); // create a new object
-    $mail->IsSMTP(); // enable SMTP
-    $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
-    $mail->SMTPAuth = true; // authentication enabled
-    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-    $mail->Host = "smtp.gmail.com";
-    $mail->Port = 465; // or 587
-    $mail->IsHTML(true);
-    $mail->Username = "authenticatorkaprakop";
-    $mail->Password = "asli!pass";
-    $mail->SetFrom("authenticatorkaprakop@gmail.com");
-    $mail->Subject = $subject;
-    $mail->Body = $body;
-    $mail->AddAddress($to);
 
-    if(!$mail->Send()) {
-        echo "Mailer Error: " . $mail->ErrorInfo;
-    }
-    else {
-        echo "Message has been sent";
-    }
-}
 ?>

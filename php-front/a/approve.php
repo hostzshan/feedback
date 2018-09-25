6d674a7d0e604891384f8c2ce321ede91821e3ab
Zeeshan <zshanakhtar@oulook.com>
2018-09-25 14:27:28 +0530

    approvet values updated

--- php-front/a/approve.php
+++ php-front/a/approve.php
@@ -2,12 +2,12 @@
     <div class="panel-heading">Applications</div>
     <div class="panel-body z-submenu">
         <ul class="nav nav-tabs" data-fragment="approvet">
-            <li class="active" data-approvet="0"><a data-toggle="tab">CSE<span class="btn btn-xs btn-danger" id="pr0_count"></span></a></li>
-            <li data-approvet="1"><a data-toggle="tab">IT<span class="btn btn-xs btn-danger" id="pr1_count"></span></a></li>
-            <li data-approvet="2"><a data-toggle="tab">ME<span class="btn btn-xs btn-danger" id="pr2_count"></span></a></li>
-            <li data-approvet="3"><a data-toggle="tab">CE<span class="btn btn-xs btn-danger" id="pr3_count"></span></a></li>
-            <li data-approvet="4"><a data-toggle="tab">EN<span class="btn btn-xs btn-danger" id="pr4_count"></span></a></li>
-            <li data-approvet="5"><a data-toggle="tab">EC<span class="btn btn-xs btn-danger" id="pr5_count"></span></a></li>
+            <li class="active" data-approvet="CSE"><a data-toggle="tab">CSE<span class="btn btn-xs btn-danger" id="pr0_count"></span></a></li>
+            <li data-approvet="IT"><a data-toggle="tab">IT<span class="btn btn-xs btn-danger" id="pr1_count"></span></a></li>
+            <li data-approvet="ME"><a data-toggle="tab">ME<span class="btn btn-xs btn-danger" id="pr2_count"></span></a></li>
+            <li data-approvet="CE"><a data-toggle="tab">CE<span class="btn btn-xs btn-danger" id="pr3_count"></span></a></li>
+            <li data-approvet="EN"><a data-toggle="tab">EN<span class="btn btn-xs btn-danger" id="pr4_count"></span></a></li>
+            <li data-approvet="EC"><a data-toggle="tab">EC<span class="btn btn-xs btn-danger" id="pr5_count"></span></a></li>
         </ul>
         <div id="approvet" class="tab-content">
             Here table of approveT

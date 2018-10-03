2dfc8ad47d8d2cf8e459970ea05f19b6b6273841
Zeeshan <zshanakhtar@oulook.com>
2018-10-03 19:44:56 +0530

    footer initialised

---
+++ php-front/footer.php
@@ -0,0 +1,23 @@
+<!-- Footer -->
+
+<div class="container">
+<marquee>
+<div class="col-xs-12">
+<h2 class="text-danger">* Site works best with latest versions of Mozilla Firefox and Google Chrome. Please do not use Internet Explorer *</h2>
+</div>
+</marquee>
+</div>
+<footer class="col-xs-12 z-footer">
+    <div class="col-xs-12 pull-right">
+      <span class="col-xs-4">
+        <span class="glyphicon glyphicon-map-marker"></span>
+        Krishna Engineering College
+        <br>Mohannagar...........
+        <br>Ghaziabad -201007
+      </span>
+      <span class="col-xs-4 text-center"><span class="glyphicon glyphicon-phone"></span> xxx-xxxxxxx</span>
+      <span class="col-xs-4 text-right"><span class="glyphicon glyphicon-envelope"></span> xxx@yyy.zzz</span> 
+  </div>
+</footer>
+
+

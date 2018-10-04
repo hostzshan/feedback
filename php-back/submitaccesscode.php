24804b2b7dd3d582d0e4cadb4ad2efdd9be9b9ab
Zeeshan <zshanakhtar@oulook.com>
2018-10-04 23:49:25 +0530

    minor fix

--- php-back/submitaccesscode.php
+++ php-back/submitaccesscode.php
@@ -7,12 +7,12 @@ $error_main='Access Code Not Generated';
 
 if ($_SERVER['REQUEST_METHOD']=='POST'){
     $_POST=realescape($_POST);//escape single and double quotes using mysqli_real_escape
-    extract($_POST);//get all post parameters
     $flag=validate($_POST);
     if($flag!='valid')
         errordisplay($error_main,$flag);
     else
     {
+        extract($_POST);//get all post parameters
         $access=$department.$course_year.$batch;
         $access_code=generateaccesscode($access);
         $data=array('access'=>$access,'access_code'=>$access_code,'allowed_users'=>$allowed_users,'description'=>$description);

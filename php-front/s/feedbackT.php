e3e4f82d42957db7107d9dc2e0534991356766c2
Zeeshan <zshanakhtar@oulook.com>
2018-10-04 23:10:48 +0530

    feedback trinket sort by faculty id

--- php-front/s/feedbackT.php
+++ php-front/s/feedbackT.php
@@ -1,6 +1,6 @@
 <?php
-$query="SELECT * from form WHERE feedback_code='$feedback_code'";
-// echo $FILLteam;
+$query="SELECT * from form WHERE feedback_code='$feedback_code' ORDER BY faculty_id";
+// echo $query;
 $resultform=mysqli_query($conn,$query);
 
 $feedback_params=array('Knowledge of subject','Communication skills');

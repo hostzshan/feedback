e890dfb7af140848d93276a87aef79a0b6627e2c
Zeeshan <zshanakhtar@oulook.com>
2018-10-04 23:07:05 +0530

    improved insert db function new update db function improved realescape

--- php-back/functions.php
+++ php-back/functions.php
@@ -2,20 +2,42 @@
 function realescape($arr){
     include "../php-back/".'connection.php';
     foreach($arr as $x => $x_value)
+    {
         $arr[$x]=mysqli_real_escape_string($conn, $x_value);
+        // Get rid of any newline characters in $string.
+        $arr[$x] = str_replace(PHP_EOL, '', $x_value);
+    }
     return $arr;
+    $conn->close();
 }
+
 function insertintodb($table,$data){
     include "../php-back/".'connection.php';
-    $keys=implode(",", array_keys($data) );
-    $values=implode("','", array_values($data) );
-    $FILLteam="INSERT INTO $table ($keys) values ('$values')";
-    // echo $FILLteam;
-    if($conn->query($FILLteam))
-        return true;
-    else
+    $query1 = "INSERT INTO $table (sno) VALUES (NULL)";
+    if ($conn->query($query1) === TRUE) {
+        $last_id = $conn->insert_id;
+        return updatedb($table,$data,$last_id)?true:false;
+    } else {
         return false;
+    }
+    $conn->close();
 }
+
+function updatedb($table,$data,$sno){
+    include "../php-back/".'connection.php';
+    $flag_update_statement=0;
+    foreach($data as $x => $x_value){
+        // echo $x."\n";
+        // echo $x_value."\n";
+        $stmt = $conn->prepare("UPDATE $table SET $x = ? WHERE sno='$sno'");
+        $stmt->bind_param("s", $x_value);
+        $flag_update_statement = $stmt->execute()?$flag_update_statement+1:$flag_update_statement;
+        $stmt->close();
+    }
+    return $flag_update_statement==4?true:false;
+    $conn->close();
+}
+
 function errordisplay($error_main,$error_desc){
     echo '<div class="alert alert-danger"><strong>'.$error_main.'!</strong> '.$error_desc.'</div>';
 }

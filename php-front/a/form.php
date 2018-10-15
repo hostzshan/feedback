b695d3f9d5dcba6aedb5927ffcfc355ba1dd0395
Zeeshan <zshanakhtar@oulook.com>
2018-10-16 00:07:55 +0530

    form adash module initialised

---
+++ php-front/a/form.php
@@ -0,0 +1,96 @@
+<?php
+$query="SELECT form_info.feedback_code FROM regist INNER JOIN access INNER JOIN form_info ON regist.access_code=access.access_code AND access.feedback_code=form_info.feedback_code WHERE regist.username='$username'";
+// echo $query;
+$resultaccess=mysqli_query($conn,$query);
+?>
+
+<div class="panel panel-danger">
+	<div class="panel-heading" data-toggle="collapse" data-target="#available" style="font-size:150%;"><b>Active</b><span class="btn btn-danger pull-right glyphicon glyphicon-chevron-up"></span></div>
+	<div  class="panel-body collapse in one" id="available">
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>S. No.</th>
+                    <th>Feedback Name</th>
+                    <th>Field(s) / Faculties</th>
+                    <th>Allowed Users</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+<?php
+while($rowaccess = $resultaccess->fetch_assoc())
+{
+?>
+                <tr>
+                    <?php
+                    $feedback_code = htmlspecialchars($rowaccess['feedback_code']);
+                    $feedback_desc = getdescription($conn,"feedback",$feedback_code);
+                    ?>
+                    <td>1</td>
+                    <td><?php echo $feedback_desc; ?></td>
+                    <td class="playground">
+                        <button type="submit" class="btn btn-danger z-optionbtn" data-target=".z-optionbox.z-i0" style="float:left;">
+                            <span class="glyphicon glyphicon-list-alt"></span>
+                        </button>
+                        <div class="col-xs-12" style="float:left;position:relative">
+                            <ul class="z-optionbox z-i0" data-requester='trinket' data-fragment='facultyt' style="display:none;">
+                                <li class="z-option" data-facultyt="edit&feedback_code=<?php echo $feedback_code; ?>" title="Open Feedback Form as Student"><span class="glyphicon glyphicon-edit"></span><br>Edit</li>
+                                <li class="z-option" data-facultyt="preview&feedback_code=<?php echo $feedback_code; ?>" title="Open Feedback Preview as Student"><span class="glyphicon glyphicon-eye-open"></span><br>Preview</li>
+                            </ul>
+                        </div>
+                    </td>
+                    <td class="playground">
+                        <button type="submit" class="btn btn-danger z-optionbtn" data-target=".z-optionbox.z-i1" style="float:left;">
+                            <span class="glyphicon glyphicon-user"></span>
+                        </button>
+                        <div class="col-xs-12" style="float:left;position:relative">
+                            <ul class="z-optionbox z-i1" data-requester='trinket' data-fragment='accesst' style="display:none;">
+                                <li class="z-option" data-accesst="edit&feedback_code=<?php echo $feedback_code; ?>" title="Open Feedback Form as Student"><span class="glyphicon glyphicon-edit"></span><br>Edit</li>
+                                <li class="z-option" data-accesst="preview&feedback_code=<?php echo $feedback_code; ?>" title="Open Feedback Preview as Student"><span class="glyphicon glyphicon-eye-open"></span><br>Preview</li>
+                            </ul>
+                        </div>
+                    </td>
+                    <td class="playground">
+                        <button type="submit" class="btn btn-danger z-optionbtn" data-target=".z-optionbox.z-i2" style="float:left;">
+                            <span class="glyphicon glyphicon-cog"></span>
+                        </button>
+                        <div class="col-xs-12" style="float:left;position:relative">
+                            <ul class="z-optionbox z-i2" data-requester='trinket' data-fragment='formt' style="display:none;">
+                                <li class="z-option" data-formt="edit&feedback_code=<?php echo $feedback_code; ?>" title="Open Feedback Form as Student"><span class="glyphicon glyphicon-edit"></span><br>Edit</li>
+                                <li class="z-option" data-formt="preview&feedback_code=<?php echo $feedback_code; ?>" title="Open Feedback Preview as Student"><span class="glyphicon glyphicon-eye-open"></span><br>Preview</li>
+                            </ul>
+                        </div>
+                    </td>
+                </tr>
+<?php 
+}
+?>
+				<tr>
+					<td colspan="10">
+						<button type="button" onclick="addmem(this)" name="" value="" class="btn btn-xs col-xs-12 btn-info" >
+							<span class="glyphicon glyphicon-plus"></span>
+							<span>Add New</span>
+						</button>			
+					</td>
+				</tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+<div class="panel panel-danger">
+	<div class="panel-heading" data-toggle="collapse" data-target="#trinket" style="font-size:150%;"><b>Feedback Form Editor</b><span class="btn btn-danger pull-right glyphicon glyphicon-chevron-up"></span></div>
+	<div  class="panel-body collapse in one" id="trinket">
+		
+	</div>
+</div>
+
+
+<script>
+    $(".z-optionbtn").zoption();
+    $(".playground").fragmentLoader("trinket");
+    
+</script>
+

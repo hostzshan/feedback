440725b47a86df781fe0aaa502fbbee636fca0ef
Zeeshan <zshanakhtar@oulook.com>
2018-10-17 01:42:24 +0530

    rename submmit faculty to submitmutiform

--- php-back/submitmultiform.php
+++ php-back/submitmultiform.php
@@ -14,20 +14,28 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
     // var_dump($_POST);
     if($usertype=='a')
     {
+        if($type=="faculty"){
+            $table='form';
+            $parameter_name='faculty_id';
+        }
+        if($type=="access"){
+            $table='access';
+            $parameter_name='access_code';
+        }
         if($action=="delete")
         {
-            $data=array('feedback_code'=>$control,'faculty_id'=>$parameter);
+            $data=array('feedback_code'=>$control, $parameter_name=>$parameter);
             // errordisplay($error_main,'Deletion disabled.');//Enable this to close deletion during development
-            if(deletefromdb('form',$data))
+            if(deletefromdb($table,$data))
                 successdisplay($success_main,'Deleted feild from this form.');
             else
                 errordisplay($error_main,'Server Error, please try again later.');
         }
         else if($action=="edit")
         {
-            $find=array('feedback_code'=>$control,'faculty_id'=>$parameter);
+            $find=array('feedback_code'=>$control, $parameter_name=>$parameter);
             $data=array('faculty_desc'=>$faculty_desc);
-            $query = "SELECT sno FROM form";
+            $query = "SELECT sno FROM ".$table;
             $i=0;
             foreach($find as $x => $x_value){
                 // echo $x."\n";
@@ -39,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
             if($row = $result->fetch_assoc()){
                 $sno=$row['sno'];
                 // errordisplay($error_main,'Deletion disabled.');//Enable this to close edit during development
-                if(updatedb('form',$data,$sno))
+                if(updatedb($table,$data,$sno))
                     successdisplay($success_main,'Edited feild description on this form.');
                 else
                     errordisplay($error_main,'Server Error, please try again later.');

9b250166ddfe03c8ff6c13af83395133dd701dc1
Zeeshan <zshanakhtar@oulook.com>
2018-10-20 12:21:54 +0530

    multiform error and success messages updated, add action implemented

--- php-back/submitmultiform.php
+++ php-back/submitmultiform.php
@@ -4,8 +4,7 @@ include 'connection.php';
 // include 'f_faculty_validate.php';
 
 session_start();
-$error_main='Faculty Action Failed';
-$success_main='Faculty Action Succeded';
+
 
 if ($_SERVER['REQUEST_METHOD']=='POST'){
     $_POST=realescape($_POST);//escape single and double quotes using mysqli_real_escape
@@ -17,10 +16,18 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
         if($type=="faculty"){
             $table='form';
             $parameter_name='faculty_id';
+            $error_main='Faculty Action Failed';
+            $success_main='Faculty Action Succeded';
         }
-        if($type=="access"){
+        else if($type=="access"){
             $table='access';
             $parameter_name='access_code';
+            $error_main='Access Action Failed';
+            $success_main='Access Action Succeded';
+        }
+        else{
+            errordisplay($error_main,'Server Error, please try again later.');
+            return false;
         }
         if($action=="delete")
         {
@@ -53,10 +60,31 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
                     errordisplay($error_main,'Server Error, please try again later.');
             }
         }
-        // if(insertintodb('regist',$data))
-        //     sendmail('zshanakhtar@outlook.com','Registration Done Successfully','You have been successfully registered please contact administrator to activate account.');
-        // else
-        //     errordisplay($error_main,'Server Error, please try again later.');
+        else if($action=="add")
+        {
+            if($type=="faculty")
+            {
+                $data=array('feedback_code'=>$control,'faculty_id'=>$faculty_id,'faculty_desc'=>$faculty_desc);
+                $success_info="Faculty inserted on this form successfully.";
+                $error_info="Faculty insertion failed.";
+            }
+            else if($type=="access")
+            {
+                $data=array('feedback_code'=>$control,'access_code'=>$access_code);
+                // var_dump($data);
+                $success_info="Access assigned to this form successfully.";
+                $error_info="Access assignation failed.";
+            }
+            else{
+                errordisplay($error_main,'Unexpected.');
+                return false;
+            }
+            // errordisplay($error_main,'Addition disabled.');//Enable this to close edit during development
+            if(insertintodb($table,$data))
+                successdisplay($success_main,$success_info);
+            else
+                errordisplay($error_main,$error_info);
+        }
     }
     else
     {

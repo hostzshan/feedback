d27cdd4d3356e4e5f4b668b379e79a344c261f90
Zeeshan <zshanakhtar@oulook.com>
2018-10-26 04:07:36 +0530

    registration validate

--- php-back/f_registration_validate.php
+++ php-back/f_registration_validate.php
@@ -2,9 +2,12 @@
 function validate($data)
 {
 extract($data);
-if( !inputvalidate('exact-length',array($univ_roll=>10)) )
-    return 'University roll incorrect length, please input 10 characters.';
+if( !inputvalidate('regex',array($univ_roll=>'num',$access_code=>'alphanum',$department=>'alpha')) )
+    return 'Invalid input / Only Alphanumeric characters(a-z, A-Z, 0-9), underscores(_) and hyphens(-) allowed.';
+if( !inputvalidate('exact-length',array($univ_roll=>10,$access_code=>9)) )
+    return 'University roll incorrect length, please input 10 characters / Access code incorrect length, please input 9 characters';
 if( !inputvalidate('min-length',array($pass=>6)) )
     return 'Password is of incorrect length, please input atleast 6 characters.';
+return 'true';
 }
 ?>

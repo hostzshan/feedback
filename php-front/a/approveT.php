c88657874775552677ff51cbb947b4fab156d737
Zeeshan <zshanakhtar@oulook.com>
2018-10-25 23:17:10 +0530

    a approve users module and trinkets

--- php-front/a/approveT.php
+++ php-front/a/approveT.php
@@ -1,3 +1,10 @@
 <?php
-echo $approvet;
+$query="SELECT DISTINCT regist.username,cluster.section,cluster.batch,regist.access from regist INNER JOIN cluster WHERE regist.access=cluster.access AND cluster.department='$approvet' AND regist.usertype='s' ORDER BY regist.username";
+// echo $query;
+$resultform=mysqli_query($conn,$query);
+
+// $feedback_params=array('Knowledge of subject','Communication skills');
+
+    include 'approveT_users.php';
+
 ?>
---
+++ php-front/a/approveT_users.php
@@ -0,0 +1,62 @@
+<?php require_once "../php-back/functions.php";$i=1; ?>
+<div id="user"></div>
+<table class="table table-bordered table-striped">
+    <thead>
+        <tr>
+            <th>S. No.</th>
+            <th>Username</th>
+            <th>Name</th>
+            <th>Batch</th>
+            <th>Section</th>
+            <th>Access</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+while($rowform = $resultform->fetch_assoc())
+{
+?>
+            <tr>
+                <?php
+                $username = htmlspecialchars($rowform['username']);
+                $username_desc = getdescription($conn,"username",$username);
+                ?>
+                <td><?php echo $i++; ?></td>
+                <td><?php echo $username; ?></td>
+                <td><?php echo $username_desc; ?></td>
+                <td><?php echo htmlspecialchars($rowform['batch']); ?></td>
+                <td><?php echo htmlspecialchars($rowform['section']); ?></td>
+                <td>
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Description" data-name="faculty_desc" value="<?php echo htmlspecialchars($rowform['access']); ?>" >
+                        <span class="input-group-addon alert-info btn btn-info zmultisubmit" type="submit" data-action="edit" data-parameter="<?php echo $username; ?>" data-type="faculty">
+                            <span class="glyphicon glyphicon-pencil"></span>
+                        </span>
+                    </div>
+                </td>
+                <td class="userstatus">
+                    <button type="submit" class="btn btn-xs btn-danger z-optionbtn" data-target=".z-optionbox.z-i0<?php echo $i;?>" style="float:left;">
+                        <span class="glyphicon glyphicon-cog"></span> Settings
+                    </button>
+                    <div class="col-xs-12" style="float:left;position:relative">
+                        <ul class="z-optionbox z-i0<?php echo $i;?>" data-requester='status' data-fragment='type' data-control='<?php echo $username; ?>' style="display:none;">
+                            <li class="z-option" data-type="user&status=t" title="Change User Status to Active"><span class="glyphicon glyphicon-ok-circle"></span> Activate</li>
+                            <li class="z-option" data-type="user&status=f" title="Change User Status to Disabled"><span class="glyphicon glyphicon-ban-circle"></span> Disable</li>
+                        </ul>
+                    </div>
+                </td>
+            </tr>
+<?php 
+}
+?>
+    </tbody>
+</table>
+
+<script>
+    $(".z-optionbtn").zoption();
+    $(".userstatus").zstatusUpdater('user');
+    // $("#control").val('<?php echo ''; ?>');
+    $(".zmultisubmit").zformmultisubmit();
+
+</script>

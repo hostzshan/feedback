45eef9f580adf94eb557f946b3dfeddaf5ce689e
Zeeshan <zshanakhtar@oulook.com>
2018-10-25 23:14:58 +0530

    access code generate validation

--- php-back/f_accesscode_validate.php
+++ php-back/f_accesscode_validate.php
@@ -1,11 +1,13 @@
 <?php
 function validate($data)
 {
-extract($_POST);
+extract($data);
 if( !inputvalidate('max-length',array($allowed_users=>3,$department=>3)) )
     return 'Allowed users/Department invalid, input max 3 characters';
-if( !inputvalidate('exact-length',array($course_year=>1,$batch=>2)) )
-    return 'Course year (should be exactly 1 character) invalid/Batch (should be exactly 2 characters) invalid';
+if( !inputvalidate('exact-length',array($batch=>4)) )
+    return 'Batch invalid, input exactly 4 characters';
+if( !inputvalidate('max-length',array($section=>1)) )
+    return 'Section invalid, maximum 1 character';
 return 'valid';
 }
 ?>
